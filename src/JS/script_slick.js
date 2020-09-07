81e3600cb4e5a66e8112b9d48405a251f78b5883
rakoth-gri <70511638+rakoth-gri@users.noreply.github.com>
2020-09-07 22:37:53 +0300

    adaptive .slick

--- src/JS/script_slick.js
+++ src/JS/script_slick.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $('.slick__slider').slick({
         infinite: true,
         slidesToShow: 2,
-        slidesToScroll: 1,
+        slidesToScroll: 2,
         speed: 700,          
         prevArrow: '<button type="button" class="slick-prev"> <img src="icons/prev.png"></button>',
         nextArrow: '<button type="button" class="slick-next"> <img src="icons/next.png"></button>'
