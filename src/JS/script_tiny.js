06539f1b16d84850bdda72d39f29a7e3984a8623
rakoth-gri <70511638+rakoth-gri@users.noreply.github.com>
2020-09-09 14:56:28 +0300

    modernised with tiny slider

---
+++ src/JS/script_tiny.js
@@ -0,0 +1,17 @@
+
+var slider = tns({
+    container: '.tiny__slider',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    controls: false,
+    nav: false    
+});
+
+document.querySelector('.prev') .addEventListener ('click', function () {
+    slider.goTo('prev');
+});
+
+document.querySelector('.next') .addEventListener ('click', function () {
+    slider.goTo('next');
+});
