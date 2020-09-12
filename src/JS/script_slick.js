1c48bcf962b373c050b71517726d9bdeb3e3cdec
rakoth-gri <70511638+rakoth-gri@users.noreply.github.com>
2020-09-12 23:14:50 +0300

    .tabs with JS DONE

--- src/JS/script_slick.js
+++ src/JS/script_slick.js
@@ -7,4 +7,27 @@ $(document).ready(function(){
         prevArrow: '<button type="button" class="slick-prev"> <img src="icons/prev.png"></button>',
         nextArrow: '<button type="button" class="slick-next"> <img src="icons/next.png"></button>'
     });
+
+    $('ul.tabs__catalogue').on('click', 'li:not(.tab_active)', function() {
+        $(this)
+          .addClass('tab_active').siblings().removeClass('tab_active')
+          .closest('div.container').find('div.tabs__element').removeClass('tabs__element_active').eq($(this).index()).addClass('tabs__element_active');
+    });
+
+    $(".item__link").each(function(i) {
+        $(this).on('click', function(e) {
+            e.preventDefault();
+            $('.item__card').eq(i).toggleClass('item__card_active');
+            $('.item__modallist').eq(i).toggleClass('item__modallist_active');
+        });
+    });
+
+    $(".back").each(function(i) {
+        $(this).on('click', function(e) {
+            e.preventDefault();
+            $('.item__card').eq(i).toggleClass('item__card_active');
+            $('.item__modallist').eq(i).toggleClass('item__modallist_active');
+        });
+    });
+
 });
